52f799e39c2f3842beb653e763763ae15115fa7d
雨中歌者 <esinger@qq.com>
2018-05-01 20:22:00 +0800

    重构了编译、开发、构建流程

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,24 @@ const gulp = require('gulp')
 const concat = require('gulp-concat')
 const rename = require('gulp-rename')
 const minify = require('gulp-clean-css')
-const autoprefixer = require('gulp-autoprefixer')
+const prefixer = require('gulp-autoprefixer')
 const sass = require('gulp-sass')
 const beautify = require('gulp-cssbeautify')
+
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 const moment = require('moment')
 const del = require('del')
 const replace = require('gulp-replace')
 
 const scssDir = './src/scss'
-const cssDir = './src/css'
+const devDir = './dev'
+const cssDir = devDir + '/css'
 const distDir = './dist'
-const demoDir = './demo'
 
 const scssFiles = scssDir + '/*.scss'
-const cssFiles = cssDir + '/*.css'
+const devFiles = devDir + '/**/*.*'
 const watchFiles = scssDir + '/**/*.scss'
 
 const autoprefixerConfig = require('./config/autoprefixer')
@@ -39,26 +42,39 @@ gulp.task('del:dist', function (cb) {
     del([distDir])
 })
 
-// 处理SCSS
-gulp.task('scss', function () {
+// 编译SCSS
+gulp.task('compile', function () {
     return gulp.src(scssFiles)
         .pipe(sass())
-        .pipe(autoprefixer(autoprefixerConfig))
+        .pipe(prefixer(autoprefixerConfig))
         .pipe(beautify())
-        .pipe(gulp.dest(cssDir))
-        .pipe(minify())
-        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(cssDir))
 })
 
-// 监听SCSS变化
+// 监视SCSS并自动编译
 gulp.task('watch', function () {
-    gulp.watch(watchFiles, ['scss'])
+    gulp.watch(watchFiles, ['compile'])
+})
+
+// 启动服务器
+gulp.task('server', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: devDir
+        }
+    })
+
+    gulp.watch(watchFiles, ['compile'])
+    gulp.watch(devFiles).on('change', reload)
 })
 
 // 构建分发版本
 gulp.task('build', function () {
-    return gulp.src(cssFiles)
+    return gulp.src(scssFiles)
+        .pipe(sass())
+        .pipe(prefixer(autoprefixerConfig))
+        .pipe(beautify())
         .pipe(replace('{name}', packageConfig.name))
         .pipe(replace('{description}', packageConfig.description))
         .pipe(replace('{version}', packageConfig.version))
@@ -66,5 +82,8 @@ gulp.task('build', function () {
         .pipe(replace('{repository}', packageConfig.repository.url))
         .pipe(replace('{homepage}', packageConfig.homepage))
         .pipe(replace('{date}', moment().format('YYYY-MM-DD hh:mm:ss')))
+        .pipe(gulp.dest(distDir))
+        .pipe(minify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(distDir))
 })
